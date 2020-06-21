8d9883ffff30143da2be266637423a130fa4c86d
MY HOMEWORK <ziyuandvip@qq.com>
2020-06-21 13:47:46 +0800

    Update C189Checkin.py

--- C189Checkin.py
+++ C189Checkin.py
@@ -26,12 +26,10 @@ def main():
     netdiskBonus = response.json()['netdiskBonus']
     if(response.json()['isSign'] == "false"):
         print(f"未签到，签到获得{netdiskBonus}M空间")
-        url = 'https://sc.ftqq.com/SCU90007Tbf56e1a2851fa801dfd7a29471692ed35e7113a4ed0e3.send?text=未签到，签到获得'{netdiskBonus}'M空间'
-        r = requests.get(url)
+        r = requests.get(https://sc.ftqq.com/SCU90007Tbf56e1a2851fa801dfd7a29471692ed35e7113a4ed0e3.send?text=未签到，签到获得{netdiskBonus}M空间)
     else:
         print(f"已经签到过了，签到获得{netdiskBonus}M空间")
-        url = 'https://sc.ftqq.com/SCU90007Tbf56e1a2851fa801dfd7a29471692ed35e7113a4ed0e3.send?text=已经签到过了，签到获得'{netdiskBonus}'M空间'
-        r = requests.get(url)
+        r = requests.get(https://sc.ftqq.com/SCU90007Tbf56e1a2851fa801dfd7a29471692ed35e7113a4ed0e3.send?text=未签到，签到获得{netdiskBonus}M空间)
     headers = {
         'User-Agent':'Mozilla/5.0 (Linux; Android 5.1.1; SM-G930K Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.136 Mobile Safari/537.36 Ecloud/8.6.3 Android/22 clientId/355325117317828 clientModel/SM-G930K imsi/460071114317824 clientChannelId/qq proVersion/1.0.6',
         "Referer" : "https://m.cloud.189.cn/zhuanti/2016/sign/index.jsp?albumBackupOpened=1",
